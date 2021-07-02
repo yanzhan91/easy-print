59f88d3298f56d796ae0b1616dadf9316a85eec1
Yan Zhan <yanzhan91@gmail.com>
2021-07-02 01:23:53 -0500

    Add initial easy-print code

---
+++ src/main/java/com/yzdevelopment/easyprint/EasyPrint.java
@@ -0,0 +1,68 @@
+package com.yzdevelopment.easyprint;
+
+import java.util.Map;
+
+@SuppressWarnings("All")
+public final class EasyPrint {
+    private EasyPrint() {}
+
+    private static boolean showPrint = true;
+    private static boolean showLineNumber = true;
+    private static boolean showType = true;
+
+    public static void enable(boolean enable) {
+        EasyPrint.showPrint = enable;
+    }
+
+    public static void setShowLineNumber(boolean showLineNumber) {
+        EasyPrint.showLineNumber = showLineNumber;
+    }
+
+    public static void setShowType(boolean showType) {
+        EasyPrint.showType = showType;
+    }
+
+    private static boolean enabled(boolean configEnabled, String envName) {
+        Map<String, String> env = System.getenv();
+        return Boolean.parseBoolean(env.get(envName)) || configEnabled;
+    }
+
+    public static <T> void print(T t) {
+        print(t, 3);
+    }
+
+    private static <T> void print(T t, int callerIndex) {
+        if (enabled(showPrint, "easyprint.enabled")) {
+
+            StringBuilder sb = new StringBuilder();
+
+            if (enabled(showLineNumber, "easyprint.showLineNumber")) {
+                StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+                StackTraceElement caller = stackTraceElements[callerIndex];
+
+                sb.append(String.format("%s > %s:%s", caller.getClassName(), caller.getMethodName(), caller.getLineNumber()));
+            }
+
+            if (enabled(showType, "easyprint.showType")) {
+                sb.append(" (" + t.getClass().getName() + ")");
+            }
+
+            sb.append(" - ").append(t.toString());
+            System.out.println(sb.toString());
+        }
+    }
+
+    public static <T> void p(T t) {
+        print(t, 3);
+    }
+
+    public static <T> T spy(T t) {
+        print(t, 3);
+        return t;
+    }
+
+    public static <T> T s(T t) {
+        print(t, 3);
+        return t;
+    }
+}
---
+++ src/test/java/com/yzdevelopment/easyprint/EasyPrintTest.java
@@ -0,0 +1,100 @@
+package com.yzdevelopment.easyprint;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static com.yzdevelopment.easyprint.EasyPrint.*;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+@SuppressWarnings("All")
+public class EasyPrintTest {
+
+    private ByteArrayOutputStream myOut;
+
+    @Before
+    public void setup() {
+        EasyPrint.enable(true);
+        myOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(myOut));
+    }
+
+    @After
+    public void teardown() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void test_print() {
+        print(123456);
+
+        assertThat(myOut.toString(), containsString("com.yzdevelopment.easyprint.EasyPrintTest"));
+        assertThat(myOut.toString(), containsString("test_print:"));
+        assertThat(myOut.toString(), containsString("123456"));
+    }
+
+    @Test
+    public void test_p() {
+        p("123456");
+
+        assertThat(myOut.toString(), containsString("com.yzdevelopment.easyprint.EasyPrintTest"));
+        assertThat(myOut.toString(), containsString("test_p:"));
+        assertThat(myOut.toString(), containsString("123456"));
+    }
+
+    @Test
+    public void test_spy() {
+        Integer result = spy(123456);
+
+        assertThat(result, is(123456));
+        assertThat(myOut.toString(), containsString("com.yzdevelopment.easyprint.EasyPrintTest"));
+        assertThat(myOut.toString(), containsString("test_spy:"));
+        assertThat(myOut.toString(), containsString("123456"));
+    }
+
+    @Test
+    public void test_s() {
+        String result = spy("123456");
+
+        assertThat(result, is("123456"));
+        assertThat(myOut.toString(), containsString("com.yzdevelopment.easyprint.EasyPrintTest"));
+        assertThat(myOut.toString(), containsString("test_s:"));
+        assertThat(myOut.toString(), containsString("123456"));
+    }
+
+    @Test
+    public void test_class() {
+
+        class SampleTestClass {
+            private final int one = 1;
+            private final String two = "2";
+
+            @Override
+            public String toString() {
+                return "Test{" +
+                        "one=" + one +
+                        ", two='" + two + '\'' +
+                        '}';
+            }
+        }
+
+        print(new SampleTestClass());
+
+        assertThat(myOut.toString(), containsString("com.yzdevelopment.easyprint.EasyPrintTest"));
+        assertThat(myOut.toString(), containsString("test_class:"));
+        assertThat(myOut.toString(), containsString("Test{one=1, two='2'}"));
+    }
+
+    @Test
+    public void test_enable_flag() {
+        EasyPrint.enable(false);
+        print(123456);
+
+        assertThat(myOut.toString(), is(""));
+    }
+}
